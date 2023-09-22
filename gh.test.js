3eb1383629455171a6b708f0ebc28a484e5ea316
Azazel test <Azazellampss@gmail.com>
2023-09-22 19:45:05 +0300

    1 commit

---
+++ gh.test.js
@@ -0,0 +1,61 @@
+let page;
+
+beforeEach(async () => {
+  page = await browser.newPage();
+});
+
+afterEach(() => {
+page.close();
+});
+
+describe("Github page tests", () => {
+  beforeEach(async () => {
+  await page.goto("https://github.com/team");
+});
+
+test("The h1 header content'", async () => {
+    const firstLink = await page.$("header div div a");
+    await firstLink.click();
+    await page.waitForSelector('h1');
+    const title2 = await page.title();
+    expect(title2).toEqual('GitHub for teams · Build like the best teams on the planet · GitHub');
+  },5000);
+
+ test("The first link attribute", async () => {
+    const actual = await page.$eval("a", link => link.getAttribute('href') );
+    expect(actual).toEqual("#start-of-content");
+  },5000);
+
+  test("The page contains Sign in button", async () => {
+    const btnSelector = ".btn-large-mktg.btn-mktg";
+    await page.waitForSelector(btnSelector, {
+      visible: true,
+    });
+    const actual = await page.$eval(btnSelector, link => link.textContent);
+    expect(actual).toContain("Get started with Team")
+  },5000);
+});
+
+describe("Three new tests", () => {
+  afterEach(() => {
+    page.close();
+  });
+
+  test("About ", async () => {
+    await page.goto("https://github.com/about");
+    const title = await page.title();
+    expect(title).toContain("About · GitHub");
+  }, 20000);
+
+  test("Shop ", async () => {
+    await page.goto("https://www.thegithubshop.com");
+    const title = await page.title();
+    expect(title).toContain("The GitHub Shop | Home");
+  }, 20000);
+
+  test("Pricing ", async () => {
+    await page.goto("https://github.com/pricing");
+    const title = await page.title();
+    expect(title).toContain("Pricing · Plans for every developer · GitHub");
+  }, 20000);
+});
